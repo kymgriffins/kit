78c3f81892523c957734925a3d5d5078e56b0a1c
kymgriffins <kimutaigriffins1960@gmail.com>
2026-02-26 12:07:18 -0500

    done

--- api/settings.py
+++ api/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', 'www.budgetndiostory.org', 'budgetndiostory.org']
 
 # Application definition
 INSTALLED_APPS = [
@@ -146,6 +146,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'https://bns.vercel.app',
     'https://*.vercel.app',
+    'https://www.budgetndiostory.org',
+    'https://budgetndiostory.org',
 ]
 
 # Allow credentials for cross-origin requests

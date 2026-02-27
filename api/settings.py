9ab878934b59fde7695d8cd89cea64dec65ab5c5
kymgriffins <kimutaigriffins1960@gmail.com>
2026-02-27 03:37:24 -0500

    localhost

--- api/settings.py
+++ api/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', 'www.budgetndiostory.org', 'budgetndiostory.org']
+    
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', 'www.budgetndiostory.org', 'budgetndiostory.org', 'http://localhost:3000',]
 
 # Application definition
 INSTALLED_APPS = [

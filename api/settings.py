c82c8a6d07cc10cbd2b14af741f9f659597132f6
kymgriffins <kimutaigriffins1960@gmail.com>
2026-02-27 04:58:09 -0500

    okay

--- api/settings.py
+++ api/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 DEBUG = True
 
     
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', 'www.budgetndiostory.org', 'budgetndiostory.org', 'http://localhost:3000',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', 'www.budgetndiostory.org', 'budgetndiostory.org']
 
 # Application definition
 INSTALLED_APPS = [
@@ -172,7 +172,3 @@ CSRF_TRUSTED_ORIGINS = [
     'https://www.budgetndiostory.org',
     'https://budgetndiostory.org',
 ]
-
-# CORS settings - Add SameSite config
-CORS_COOKIE_NAME = 'sessionid'
-CORS_COOKIE_SAMESITE = 'Lax'
